8b9657a60d3441afb41aa48fc996f08a4af03508
k10ps <kruczy.koniec0@gmail.com>
2025-01-01 18:16:01 +0100

    Reapply "dodalam superuser (admin) do bazy"
    This reverts commit 4dc5c6237a9ffb90b9c659c52959d6bbc4d77e23.

--- Ale_Baza/products/admin.py
+++ Ale_Baza/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Listaproduktow
 
-# Register your models here.
+class AdminProdukt(admin.ModelAdmin):
+    list_display = ('id','kategoria', 'marka', 'model')
+
+admin.site.register(Listaproduktow, AdminProdukt)
